cd2bb804cd3d116bf3a8bf931cafdce806c7061c
JGoodin-01 <jakegoodin.01@gmail.com>
2023-02-19 16:06:29 -0500

    Get Basic Docker functionality in

--- parserExample.py
+++ parserExample.py
@@ -1,10 +1,22 @@
 import Course
 import Student
 import PDFParser
+import os
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 from pathlib import Path
 
-parser = PDFParser.PDFParser()
-parser.parseInfo("jacobDPR.pdf")
-for line in range (len(parser.student.coursesTaken)):
-	print (parser.student.coursesTaken[line])
+@app.route('/')
+def runExample():
+	parser = PDFParser.PDFParser()
+	parser.parseInfo("jacobDPR.pdf")
+	# return parser.student.toJSON()
+	for line in range (len(parser.student.coursesTaken)):
+		courses = courses + (parser.student.coursesTaken[line])
+	return courses
