23927c98a50c5dce725edd97f5b29d37a9929bfd
JGoodin-01 <jakegoodin.01@gmail.com>
2023-03-11 14:10:09 -0500

    Enable CORS

--- parseEndpoint.py
+++ parseEndpoint.py
@@ -6,17 +6,16 @@ import os
 import time
 
 import redis
+from flask_cors import CORS
 from flask import Flask
 
 app = Flask(__name__)
+CORS(app, resources=r'/*')
 cache = redis.Redis(host='redis', port=6379)
 
-@app.route('/parse', methods=['POST'])
+@app.route('/parse', methods=['GET'])
 def runExample():
-    try:
-        parser = PDFParser.PDFParser()
-        parser.parseInfo("jacobDPR.pdf")
+	parser = PDFParser.PDFParser()
+	parser.parseInfo("jacobDPR.pdf")
 
-        return parser.student.toJSON()
-    except:
-        return "Exception Occured"
+	return parser.student.toJSON()
