91e6065a50d0d3cabb7e5322eb9b3924c92749ab
JGoodin-01 <jakegoodin.01@gmail.com>
2023-03-12 18:49:45 -0400

    Allow POST request for custom transcript

--- parseEndpoint.py
+++ parseEndpoint.py
@@ -2,20 +2,28 @@ from pathlib import Path
 import Course
 import Student
 import PDFParser
-import os
+import os, base64
 import time
+import logging
 
 import redis
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, request
+
 
 app = Flask(__name__)
 CORS(app, resources=r'/*')
 cache = redis.Redis(host='redis', port=6379)
 
-@app.route('/parse', methods=['GET'])
+@app.route('/parse', methods=['POST'])
 def runExample():
 	parser = PDFParser.PDFParser()
-	parser.parseInfo("jacobDPR.pdf")
-
-	return parser.student.toJSON()
+	pdfData = request.files.get('uploadfile')
+	if pdfData == None:
+		parser.parseInfo("jacobDPR.pdf")
+		return parser.student.toJSON()
+	else:
+		pdfData.save("uploadedPDF.pdf")
+		parser.parseInfo("uploadedPDF.pdf")
+		return parser.student.toJSON()
+	
